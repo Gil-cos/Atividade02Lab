d97abdae6264091509646601aabb9410002cf72e
Alexandre Murata <alexandremurata@gmail.com>
2019-09-19 19:36:20 -0300

    Teste Unitario Service

--- src/test/java/com/fatec/lab/atividade02/service/AccountServiceTest.java
+++ src/test/java/com/fatec/lab/atividade02/service/AccountServiceTest.java
@@ -3,6 +3,8 @@ package com.fatec.lab.atividade02.service;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
+import java.math.BigDecimal;
+
 import javax.transaction.Transactional;
 
 import org.junit.Test;
@@ -13,20 +15,36 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fatec.lab.atividade02.entity.Account;
+import com.fatec.lab.atividade02.entity.Bank;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 @Rollback
 public class AccountServiceTest {
-	
+
 	@Autowired
 	private AccountService service;
-	
-	/* TODO
-	 * @Test public void createAccountTest() { Account account =
-	 * service.createAccount(10l, "Gilberto"); assertNotNull(account); }
-	 */	
+
+	@Test
+	public void createAccountTest() {
+		Account account = new Account();
+
+		Bank bank = new Bank();
+		bank.setId(1L);
+		bank.setName("Inter");
+
+		account.setId(1L);
+		account.setBalance(BigDecimal.TEN);
+		account.setNumber(123L);
+		account.setOwner("Jilberto Costa");
+		account.setPassword("Gsw190");
+		account.setBank(bank);
+		service.createAccount(account, 1L);
+		assertNotNull(account);
+
+	}
+
 	@Test
 	public void creteAccountNullTest() {
 		Account account = service.createAccount(null, null);
