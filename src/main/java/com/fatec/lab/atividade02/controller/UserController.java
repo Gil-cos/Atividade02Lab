933fcc3f21837f249da6392e6637412e5c398d15
gilberto.junior <gilberto.junior@gsw.com.br>
2019-11-07 23:08:20 -0300

    Fix login

--- src/main/java/com/fatec/lab/atividade02/controller/UserController.java
+++ src/main/java/com/fatec/lab/atividade02/controller/UserController.java
@@ -2,6 +2,7 @@ package com.fatec.lab.atividade02.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,19 +59,13 @@ public class UserController {
 		return ResponseEntity.created(uri).body(newUser);
 	}
 	
-//	@Transactional
-//	@PutMapping(value = "/{id}")
-//	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
-//	public ResponseEntity<UserDto> updateUser(@RequestBody @Valid UserForm userForm, @PathVariable Long id) {
-//
-//		Optional<User> optional =  userService.findById(id);
-//		if (optional.isPresent()) {
-//			User user = userForm.update(id, userService, passwordEncoder);
-//			return ResponseEntity.ok(new UserDto(user));
-//		}
-//		
-//		return ResponseEntity.notFound().build();
-//	}
+	@Transactional
+	@PutMapping(value = "/{id}")
+	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
+	@JsonView({ UserView.UserDetail.class })
+	public ResponseEntity<User> updateUser(@RequestBody @Valid UserForm userForm, @PathVariable Long id) {
+		return ResponseEntity.ok(userService.update(id, userForm));
+	}
 //	
 //	@DeleteMapping(value = "/{id}")
 //	@PreAuthorize("hasAnyRole('ROLE_ADMIN')")
--- src/main/java/com/fatec/lab/atividade02/service/UserService.java
+++ src/main/java/com/fatec/lab/atividade02/service/UserService.java
@@ -3,6 +3,8 @@ package com.fatec.lab.atividade02.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -49,5 +51,10 @@ public class UserService {
 
 	private User save(User user) {
 		return userRepository.save(user);
+	}
+
+	public User update(Long id, @Valid UserForm userForm) {
+
+		return null;
 	}
 }
